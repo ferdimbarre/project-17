eeb2ef1651ad4ee01a02e625d9ec13a016b23c78
Fernanda <fernanda19@usf.edu>
2024-11-24 13:22:05 -0500

    Implement tour fetching and rendering with state management.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Gallery from './Gallery'
-console.log(require('./Gallery'));
+import Gallery from './Gallery.jsx'
+
 
 
 function App() {
